2a698ce677efa7417dd443d3a43e4bf8f8c06c2b
AstralCT <56379127+AstralCT@users.noreply.github.com>
2020-03-30 05:13:12 -0400

    basic setup

---
+++ life.Altis/Functions.h
@@ -0,0 +1,16 @@
+// This is where you declare functions.
+
+class dev_core {
+	tag = "DEV";
+
+	class DevExampleClass {
+		file = "core";
+		class exampleScript {};
+		class scriptName {};
+	};
+
+	class DevExClass2 {
+		file = "core\scripts";
+		class jumpScript {};
+	};
+};
---
+++ life.Altis/init.sqf
@@ -0,0 +1,5 @@
+//init.sqf file
+// Everything in this file is immediatly executed by the server, and all players when the mission starts.
+
+// applying jumpScript to all players here.
+[] spawn DEV_fnc_jumpScript;
